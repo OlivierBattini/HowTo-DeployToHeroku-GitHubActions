3226261639dcab9c49a456d91b0618628c536507
Olivier BATTINI <olivier@olivierbattini.fr>
2022-09-28 19:55:18 +0200

    ADD logging

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const env = require('./config/env');
 
 // From : https://devcenter.heroku.com/articles/connecting-heroku-postgres#connecting-in-node-js
 
+console.log('========== CONFIGURING PG CLIENT');
 const client = new Client({
   connectionString: env.getDatabaseUrl(),
   ssl: {
@@ -11,8 +12,10 @@ const client = new Client({
   }
 });
 
+console.log('========== CONNECTING PG CLIENT');
 client.connect();
 
+console.log('========== QUERYING');
 client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
